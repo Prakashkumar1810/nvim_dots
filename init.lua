3e13dd9026f04d8c92c0ad482cb90d458c4e1da2
Prakash <1810prakashkumar@gmail.com>
2022-10-16 14:59:07 +0530

    Min working

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.colorscheme")
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.gitsigns"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.nvim-tree"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.alpha"
+require "user.whichkey"
